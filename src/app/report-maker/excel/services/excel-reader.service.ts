d3087239a2d205f0b16e8bc0e1d97de653862f00
johngrotts <johnsmaillist@gmail.com>
2024-08-20 16:46:35 -0500

    More on reader

--- src/app/report-maker/excel/services/excel-reader.service.ts
+++ src/app/report-maker/excel/services/excel-reader.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+import * as ExcelJS from 'exceljs';
 import { ExcelTemplateReaderProperties } from '../models/excel-template-reader-properties';
+import { ExcelMergeCells } from '../models/excel-data-content';
+import { TextUtilService } from '../../../common/utils/text-util.service';
+import { ExcelColumnProperty, ExcelFormatter, ExcelSheetFormatter, ExcelWorksheetFormatter } from '../models/excel-formatter';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +27,66 @@ export class ExcelReaderService {
   }
 
   protected static readDataFromFile(data: any, templateProperties?: ExcelTemplateReaderProperties): void {
-    
+    const excelFormatter = new ExcelFormatter();
+
+    const workbook = new ExcelJS.Workbook();
+    workbook.xlsx.load(data).then(wb => {
+      excelFormatter.sheetFormatters = [];
+      wb.eachSheet((sheet, id) => {
+        const so = sheet as any;
+
+        // Worksheet Properties
+        const sp = new ExcelWorksheetFormatter();
+        sp.sheetName = so['name'];
+        sp.headerFooter = so.headerFooter;
+
+        // Column Properties
+        const cp: ExcelColumnProperty[] = [];
+        so.columns.forEach((c: any, i: number) => {
+          const col = new ExcelColumnProperty();
+          col.column = i + 1;
+          col.width = c.width;
+          col.styles = c.style;
+          cp.push(col);
+        });
+
+        // Sheet Properties
+        const sf = new ExcelSheetFormatter();
+        sf.styles = so['pageSetup'];
+        sf.columnProperties = cp;
+
+        // Cell Merges
+        const merges = so['_merges']
+
+
+        if(templateProperties) {
+          // Initial Setup
+
+          // Sheet Head Data
+
+          // Table Header and Data
+
+          // Sheet Foot
+
+          // Individual Formulas
+
+          // Conditional Formatting
+
+
+
+        } else { // Just extract everything
+
+        }
+      });
+    });
+  }
+
+  protected static createMerge(mergeCell: any): ExcelMergeCells {
+    const emc = new ExcelMergeCells();
+    emc.firstRow = mergeCell.model.top;
+    emc.lastRow = mergeCell.model.bottom;
+    emc.firstColumn = TextUtilService.convertNumberToAlpha(mergeCell.model.left);
+    emc.lastColumn = TextUtilService.convertNumberToAlpha(mergeCell.model.right);
+    return emc;
   }
 }
